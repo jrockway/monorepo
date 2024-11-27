03d135bdc7e91f3f51d5cc7c3f3322992fc15af7
June Rockway <2367+jrockway@users.noreply.github.com>
2024-11-27 14:28:47 +0000

    initial import

---
+++ README.md
@@ -0,0 +1,10 @@
+# jrockway's monorepo
+
+I'm tired of maintaining a separate repo for each of my projects. So I'm gradually moving them into
+one.
+
+## Building
+
+Install [Bazelisk](https://github.com/bazelbuild/bazelisk), then:
+
+    bazel test ...
---
+++ workspace_status.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -euo pipefail
+
+commit_sha=$(git rev-parse HEAD)
+echo "COMMIT_SHA $commit_sha"
+
+git_branch=$(git rev-parse --abbrev-ref HEAD)
+echo "GIT_BRANCH $git_branch"
+
+git_tree_status=$(git diff-index --quiet HEAD -- && echo 'Clean' || echo 'Modified')
+echo "GIT_TREE_STATUS $git_tree_status"
