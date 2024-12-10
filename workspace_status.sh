2dfb6ae7eb91217772e34e7bb00701d34484be8c
June Rockway <2367+jrockway@users.noreply.github.com>
2024-12-10 11:53:44 +0000

    build images for ekglue

--- workspace_status.sh
+++ workspace_status.sh
@@ -10,3 +10,12 @@ echo "GIT_BRANCH $git_branch"
 
 git_tree_status=$(git diff-index --quiet HEAD -- && echo 'Clean' || echo 'Modified')
 echo "GIT_TREE_STATUS $git_tree_status"
+
+SHA256SUM="sha256sum"
+if command -v shasum &> /dev/null; then
+    # Mac OS ships shasum instead of sha256sum.
+    SHA256SUM="shasum -a 256"
+fi
+
+ekglue_version="v0.0.70-pre.g$(git describe --match=ekglue-* --long --always --dirty=".$(git diff HEAD | $SHA256SUM | cut -c 1-10)")"
+echo "EKGLUE_VERSION ${ekglue_version}"
