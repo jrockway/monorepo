e59f908764cba946c0228a08f758cec30a418b87
June Rockway <2367+jrockway@users.noreply.github.com>
2024-12-21 09:46:26 -0500

    avoid the need to patch /bin/sh

--- internal/testpostgres/testpostgres.go
+++ internal/testpostgres/testpostgres.go
@@ -2,6 +2,7 @@
 package testpostgres
 
 import (
+	"bytes"
 	"context"
 	"io/fs"
 	"os"
@@ -34,13 +35,12 @@ func postgresBin(root string, binary string) string {
 	return filepath.Join(root, "usr", "lib", "postgresql", "17", "bin", binary)
 }
 
-func postgresLib(root string) string {
-	// TODO: macos
+func postgresRpath(root string) string {
 	os := "x86_64-linux-gnu"
 	if runtime.GOARCH == "arm64" {
 		os = "aarch64-linux-gnu"
 	}
-	return "LD_LIBRARY_PATH=" + strings.Join([]string{
+	return strings.Join([]string{
 		filepath.Join(root, "lib", os),
 		filepath.Join(root, "usr", "lib", os),
 	}, ":")
@@ -49,16 +49,16 @@ func postgresLib(root string) string {
 func localLdSo(root string) string {
 	switch runtime.GOARCH {
 	case "amd64":
-		return filepath.Join(root, "/lib64/ld-linux-x86-64.so.2")
+		return filepath.Join(root, "lib64", "ld-linux-x86-64.so.2")
 	case "arm64":
-		return filepath.Join(root, "/lib/ld-linux-aarch64.so.1")
+		return filepath.Join(root, "lib", "ld-linux-aarch64.so.1")
 	}
 	return ""
 }
 
 var patchelf = ""
 
-func patch(ctx context.Context, interp, path string) error {
+func patch(ctx context.Context, interp, rpath, path string) error {
 	if patchelf == "" {
 		var err error
 		patchelf, err = runfiles.Rlocation(patchelfRlocation)
@@ -66,11 +66,25 @@ func patch(ctx context.Context, interp, path string) error {
 			return errors.Wrap(err, "find patchelf binary")
 		}
 	}
-	cmd := exec.CommandContext(ctx, patchelf, "--set-interpreter", interp, "--no-default-lib", path)
+	var args []string
+	if interp != "" {
+		args = append(args, "--set-interpreter", interp)
+	}
+	if rpath != "" {
+		args = append(args, "--set-rpath", rpath)
+	}
+	args = append(args, path)
+	cmd := exec.CommandContext(ctx, patchelf, args...)
 	cmd.Stdout = log.NewWriterAt(ctx, log.DebugLevel)
-	cmd.Stderr = log.NewWriterAt(ctx, log.DebugLevel)
+	errBuf := new(bytes.Buffer)
+	cmd.Stderr = errBuf
 	if err := cmd.Run(); err != nil {
-		return errors.Wrap(err, "run patchelf")
+		msg := errBuf.String()
+		msg = strings.ReplaceAll(msg, "\n", "; ")
+		if strings.Contains(msg, "not an ELF executable") {
+			return nil
+		}
+		return errors.Wrapf(err, "run patchelf (%v)", errBuf.String())
 	}
 	return nil
 }
@@ -87,8 +101,9 @@ func Unpack(ctx context.Context, name, rlocation string) (string, error) {
 		return root, nil
 	}
 	interp := localLdSo(root)
+	rpath := postgresRpath(root)
 	// The binaries haven't been patched yet.  Patch them.
-	bin := postgresBin(root, "")
+	bin := root // postgresBin(root, "")
 	if err := filepath.Walk(bin, func(path string, info fs.FileInfo, err error) error {
 		if err != nil {
 			return errors.Wrap(err, "WalkFn called with error")
@@ -99,8 +114,22 @@ func Unpack(ctx context.Context, name, rlocation string) (string, error) {
 		if info.IsDir() {
 			return nil
 		}
-		if err := patch(ctx, interp, path); err != nil {
-			return errors.Wrapf(err, "patch %v", path)
+		if !info.Mode().IsRegular() {
+			return nil
+		}
+		if strings.Contains(path, "ld-linux") {
+			// Patching ld.so causes Bad Things to occur.
+			return nil
+		}
+		if strings.Contains(path, "/bin/") {
+			if err := patch(ctx, interp, rpath, path); err != nil {
+				log.Debug(ctx, "failed to patch binary", zap.String("path", path), zap.Error(err))
+			}
+		}
+		if strings.Contains(filepath.Base(path), ".so") {
+			if err := patch(ctx, "", rpath, path); err != nil {
+				log.Debug(ctx, "failed to patch library", zap.String("path", path), zap.Error(err))
+			}
 		}
 		return nil
 	}); err != nil {
@@ -123,7 +152,6 @@ func PsqlCmd(ctx context.Context) (*exec.Cmd, error) {
 		return nil, errors.Wrap(err, "unpack postgres")
 	}
 	cmd := exec.CommandContext(ctx, postgresBin(root, "psql"))
-	cmd.Env = []string{postgresLib(root)}
 	return cmd, nil
 }
 
@@ -134,9 +162,6 @@ func RunPostgres(ctx context.Context) (cfg *pgx.ConnConfig, awaitCleanup func(),
 	root, err := Unpack(ctx, "postgres", postgresArchiveRlocation)
 	if err != nil {
 		return nil, nil, errors.Wrap(err, "unpack postgres")
-	}
-	if err := patch(ctx, localLdSo(root), filepath.Join(root, "bin", "sh")); err != nil {
-		return nil, nil, errors.Wrap(err, "fix ./bin/sh interpreter")
 	}
 
 	// Make tmpdir with data/config.
@@ -157,27 +182,14 @@ func RunPostgres(ctx context.Context) (cfg *pgx.ConnConfig, awaitCleanup func(),
 		}
 	}()
 
-	// CI runs as root, which postgres hates, so we drop privileges to make it start up cleanly.
-	// It also wants to run /bin/sh, so we just replace /bin/sh with the copy we bundle.  This
-	// is a little rude.  (This is working around the fact that the vendored ld-linux.so.2
-	// requires a newer libc than exists on CI machines.  Not sure how to override library
-	// loading for ld itself.)
+	// CI runs as root, which postgres hates, so we drop privileges if root to make it start up
+	// cleanly.
 	attrs := &syscall.SysProcAttr{}
 	amRoot := os.Getuid() == 0
 	if amRoot {
 		// Change ownership of the data directory to the user we're going to switch to.
 		if err := os.Chown(dir, debianNobody, debianNobody); err != nil {
 			return nil, nil, errors.Wrap(err, "chown(nobody, nobody) data tmpdir")
-		}
-
-		// Move our shell in.
-		if _, err := os.Stat("/bin/sh.orig"); err != nil && os.IsNotExist(err) {
-			if err := os.Rename("/bin/sh", "/bin/sh.orig"); err != nil {
-				return nil, nil, errors.Wrap(err, "back up /bin/sh")
-			}
-			if err := os.Symlink(filepath.Join(root, "bin", "sh"), "/bin/sh"); err != nil {
-				return nil, nil, errors.Wrap(err, "symlink in /bin/sh")
-			}
 		}
 
 		// Setup for chroot/setuid.
@@ -201,11 +213,11 @@ func RunPostgres(ctx context.Context) (cfg *pgx.ConnConfig, awaitCleanup func(),
 		"-c", "fsync=off", // huge performance drain during parallel tests
 		"-c", "unix_socket_directories="+dir,
 		"-U", "postgres",
+		"--no-locale",
 	)
 	init.Stdout = log.NewWriterAt(pctx.Child(ctx, "initdb.stdout"), log.DebugLevel)
 	init.Stderr = log.NewWriterAt(pctx.Child(ctx, "initdb.stderr"), log.DebugLevel)
-	init.Env = []string{postgresLib(root), "PATH=" + filepath.Join(root, "bin")}
-	init.SysProcAttr = attrs // postgres is very mad about being root, but CI is root!
+	init.SysProcAttr = attrs
 	log.Debug(ctx, "initializing database")
 	if err := init.Run(); err != nil {
 		return nil, nil, errors.Wrap(err, "init postgres database")
@@ -216,7 +228,6 @@ func RunPostgres(ctx context.Context) (cfg *pgx.ConnConfig, awaitCleanup func(),
 	serve := exec.CommandContext(ctx, postgresBin(root, "postgres"), "-D", dir)
 	serve.Stdout = log.NewWriterAt(pctx.Child(ctx, "stdout"), log.DebugLevel)
 	serve.Stderr = log.NewWriterAt(pctx.Child(ctx, "stderr"), log.DebugLevel)
-	serve.Env = []string{postgresLib(root)}
 	serve.SysProcAttr = attrs
 	if err := serve.Start(); err != nil {
 		return nil, nil, errors.Wrap(err, "start postgres")
